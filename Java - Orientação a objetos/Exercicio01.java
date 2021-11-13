fec13ac75e3f8293c7bebf13c72d948821777c45
Yasmim Marques <yasmim.msantos@outlook.com>
2021-11-13 20:55:52 -0300

    Orientação a objetos - Java

---
+++ Java - Orientação a objetos/Exercicio01.java
@@ -0,0 +1,28 @@
+package exercicios;
+
+public class Exercicio01 {
+	public static void main (String [] args) {}
+	
+	//1-Crie uma classe cliente e apresente os atributos e métodos referentes esta classe, em seguida crie um objeto cliente, defina as instancias deste objeto
+	//e apresente as informações deste objeto no console.		
+			
+					//Atributos
+					public String nome;
+					public int idade;
+					public String cpf;
+					
+					public boolean negativado = false;
+					
+					//Lista o cliente
+					public void listaCliente(){
+						System.out.println(nome);
+					}
+					//Lista o cliente negativado
+					public void listarClienteNegativado(){
+						negativado = true;
+					}
+					//Lista o cliente positivo
+					public void listarClientePositivo(){
+						negativado = false;	
+	}
+}
